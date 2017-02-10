6fd477a8b7dc399d3e1ac0478e2a7a5caef52500
Enrique Gonzalez <enrique@saucelabs.com>
2017-02-10 14:05:47 -0800

    First addition

---
+++ src/test/java/MySuperTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MySuperTest {
+
+    RemoteWebDriver driver;
+    private final String SAUCE_USERNAME = System.getenv("SAUCE_USERNAME");
+    private final String SAUCE_ACCESS_KEY = System.getenv("SAUCE_ACCESS_KEY");
+    private final String SAUCE_URL = String.format("https://%s:%s@ondemand.saucelabs.com:443/wd/hub",SAUCE_USERNAME,SAUCE_ACCESS_KEY);
+
+
+    @Test
+    public void testJenkinsCaps() {
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+        caps.setBrowserName(System.getenv("SELENIUM_BROWSER"));
+        caps.setVersion(System.getenv("SELENIUM_VERSION"));
+        caps.setCapability(CapabilityType.PLATFORM, "Windows 7");
+
+
+        try {
+            driver = new RemoteWebDriver(new URL(SAUCE_URL),caps);
+            driver.get("https://google.com");
+
+            System.out.println(driver.getTitle());
+            driver.quit();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
